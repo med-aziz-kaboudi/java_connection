956eff4f0d344087a53c6ae02e6afd666c4ba22a
aziz kaboudi <134620611+med-aziz-kaboudi@users.noreply.github.com>
2024-02-09 18:38:28 +0100

    new one

--- src/main/java/services/PersonneServices.java
+++ src/main/java/services/PersonneServices.java
@@ -2,40 +2,47 @@ package services;
 
 import models.Personne;
 import utils.MyDataBase;
-
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.List;
 
 public class PersonneServices implements Iservice<Personne> {
-   private Connection connection;
-    public PersonneServices(){
+    private Connection connection;
+
+    public PersonneServices() {
         connection = MyDataBase.getInstance().getconn();
     }
 
     @Override
     public void ajouter(Personne personne) throws SQLException {
-     String req = "insert into client (nom, prenom ,age) " +
-             "values('" + personne.getNom() + "','" + personne.getPrenom() + "'"
-             +  "," + personne.getAge() + ")";
-     Statement ste = connection.createStatement();
-     ste.executeUpdate(req);
+        String req = "INSERT INTO client (nom, prenom, age) VALUES('" + personne.getNom() + "','" + personne.getPrenom() + "'," + personne.getAge() + ")";
+        Statement ste = connection.createStatement();
+        ste.executeUpdate(req);
     }
 
     @Override
     public void modifier(Personne personne) throws SQLException {
-
+        String req = "UPDATE client SET nom = ?, prenom = ?, age = ? WHERE cin = ?";
+        PreparedStatement pst = connection.prepareStatement(req);
+        pst.setString(1, personne.getNom());
+        pst.setString(2, personne.getPrenom());
+        pst.setInt(3, personne.getAge());
+        pst.setInt(4, personne.getId());
+        pst.executeUpdate();
     }
 
     @Override
     public void supprimer(int id) throws SQLException {
-
+        String req = "DELETE FROM client WHERE cin = ?";
+        PreparedStatement pst = connection.prepareStatement(req);
+        pst.setInt(1, id);
+        pst.executeUpdate();
     }
 
     @Override
     public List<Personne> afficher() throws SQLException {
         return null;
     }
-
 }
--- src/main/java/tests/Test.java
+++ src/main/java/tests/Test.java
@@ -3,20 +3,31 @@ package tests;
 import models.Personne;
 import services.PersonneServices;
 import utils.MyDataBase;
-
 import java.sql.SQLException;
 
 public class Test {
-    public static void main (String [] args ){
+    public static void main(String[] args) {
         MyDataBase bd = MyDataBase.getInstance();
         PersonneServices ps = new PersonneServices();
+
         try {
-            ps.ajouter(new Personne(24,"essid","fadhel"));
+
+            Personne newPerson = new Personne(30, "hi", "fadhel");
+            ps.ajouter(newPerson);
+
+
+            int personId = 6;
+            Personne personToUpdate = new Personne(personId,30,"wassim","aziz");
+            ps.modifier(personToUpdate);
+            System.out.println("Person updated successfully.");
+
+         
+            int personIdToDelete = 1;
+             ps.supprimer(personIdToDelete);
+            System.out.println("Person deleted successfully.");
         } catch (SQLException e) {
+            e.printStackTrace();
             throw new RuntimeException(e);
         }
-
     }
 }
-
-
